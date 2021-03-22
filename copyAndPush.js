7abb6df9c427f4c824296b7dfbb248a27e29fc06
Lori Winston <Lcwinston@gmail.com>
2021-03-22 16:15:52 -0700

    spread and append

--- copyAndPush.js
+++ copyAndPush.js
@@ -3,4 +3,22 @@ const copyAndPush = (arr, num) => {
   return arr;
 };
 
-module.exports = copyAndPush; 
+module.exports = copyAndPush;
+
+const copyAndPushReturnCopy = (arr, num) => {
+  const originalArr = arr.slice();
+  originalArr.push(num);
+
+  return originalArr;
+};
+
+module.exports = copyAndPushReturnCopy;
+
+const copyWithSpread = (arr, num) => {
+  const originalArr = arr.slice();
+  const combined = [...originalArr, num];
+
+  return combined;
+};
+
+module.exports = copyWithSpread;
--- copyAndPush.test.js
+++ copyAndPush.test.js
@@ -11,3 +11,32 @@ describe('function that returns a new array with all the items in the original a
     expect(name).toEqual([1, 2, 3, 4]);
   });
 });
+
+// return a copy
+
+const copyAndPushReturnCopy = require('./copyAndPush');
+describe('return copy', () => {
+  it('pushes number to end and returns a copy', () => {
+    const numbers = [1, 2, 3];
+
+    const name = copyAndPushReturnCopy(numbers, 4);
+
+    expect(name).toEqual([1, 2, 3, 4]);
+    expect(numbers).toEqual([1, 2, 3]);
+  });
+});
+
+// return a copy with spread
+
+const copyWithSpread = require('./copyAndPush');
+
+describe('returns a copy with spread', () => {
+  it('pushes number to end and returns copy using spread', () => {
+    const numbers = [1, 2, 3];
+        
+    const name = copyWithSpread(numbers, 4);
+
+    expect(name).toEqual([1, 2, 3, 4]);
+    expect(numbers).toEqual([1, 2, 3]);
+  });
+});
